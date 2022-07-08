6ace03d7e0eb95a14c61ada56ab6a337d54d77dc
Aratrik-02 <75318952+Aratrik-02@users.noreply.github.com>
2022-07-08 22:38:01 +0530

    Changes to json files

--- app.js
+++ app.js
@@ -144,10 +144,11 @@ app.get("/:topic", function(req,res){
   })
 })
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
-
-app.listen(process.env.port || 3000, function() {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});        
